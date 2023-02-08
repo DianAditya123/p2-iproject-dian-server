3307ab27f4ca8af241c4825edf9ca5f8427f90b4
unknown <dianaditya200@gmail.com>
2023-02-08 17:17:08 +0700

    maybe done

--- controllers/home.js
+++ controllers/home.js
@@ -37,6 +37,36 @@ class homeController{
             console.log(error);
         }
     }
+
+    static async midtrans(req, res, next) {
+        let data = Type.findAll()
+        try {
+            const user = await User.findByPk(req.user.id)
+            let snap = new midtransClient.Snap({
+                isProduction: false,
+                serverKey: process.env.MIDTRANS_SERVER_KEY
+            });
+
+            let parameter = {
+                "transaction_details": {
+                    "order_id": "TRANSACTION_" + Math.floor(1000000 + Math.random() * 9000000),
+                    "gross_amount": 10000
+                },
+                "credit_card": {
+                    "secure": true
+                },
+                "customer_details": {
+                    "username": user.username,
+                    "email": user.email,
+                    "phone": user.phoneNumber
+                }
+            };
+            let token = await snap.createTransaction(parameter)
+            res.status(201).json(token)
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = homeController
